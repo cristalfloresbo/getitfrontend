684707292a1a04710525146820ca44c80d28572f
Albert Rocha Alvarado <escort84.ara@gmail.com>
2021-04-29 03:02:18 +0000

    Update home.page.ts

--- getIt/src/app/home/home.page.ts
+++ getIt/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  defaultDate = "2002-01-01";
 
-  constructor() {}
+  constructor(public formBuilder: FormBuilder) {}
+    
+  user = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(4)]],
+    lastName: ['', [Validators.required, Validators.minLength(4)]],
+    linkNum: ['', [Validators.required, Validators.minLength(32)]],
+    dateOfBirth: [this.defaultDate, [Validators.required, Validators.minLength(4)]],
+    address: ['', [Validators.required, Validators.minLength(4)]],
+    workArea: ['', [Validators.required, Validators.minLength(4)]],
+    email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+    password: ['', [Validators.required, Validators.minLength(8),Validators.pattern('[a-z0-9._%+-]+[A-Z0-9.-]')]],
+  });
+  
+    ngOnInit(){}
+
+  saveDate(){
+    console.log(this.user.value);
+  }
+
+  getDate(e) {
+    let date = new Date(e.target.value).toISOString().substring(0, 10);
+    this.user.get('dateOfBirth').setValue(date, {
+      onlyself: true
+    })
+  }
 
 }
