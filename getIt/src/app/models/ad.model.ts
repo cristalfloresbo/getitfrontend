9a9625dcddbd72d40ab078a10069d293a25e7339
Carla Marcela Guzman Miranda <carla.guzman@fundacion-jala.org>
2021-05-08 06:12:20 -0600

    api connection getallads

--- getIt/src/app/models/ad.model.ts
+++ getIt/src/app/models/ad.model.ts
@@ -1,14 +1,13 @@
 import { User } from './user.model';
 
 export interface Ad {
-    adId: string;
-    title: string;
-    imageURL: string;
-	description: string;
-	heading: string;
+    adId: number;
 	type: string;
-	requiredTime: string;
-	fee: string;
-	postDate: string;
+	workAreaName: string;
+	fee: number; 
+	address: string;
+	requiredTime: number; 
+	description: string;
+	createdAt: string;
 	user: User;
 }
